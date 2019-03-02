b15b7ba60b16d89fe7dab6d886a19c92bb192203
Jack Tilley <47064848+Jack-Tilley@users.noreply.github.com>
2019-03-02 16:07:15 -0500

    Update tablescraper.py
    added methods to get tbody and thead, reformatted get_row_info method so tables could be read easier.

--- tablescraper.py
+++ tablescraper.py
@@ -5,10 +5,11 @@
 
 from bs4 import BeautifulSoup
 
+
 class TableScrape:
     def __init__(self, soupscope, ignore_rows=[], ignore_columns=[], separated_header=True,
                  header_row_container="thead", header_row_tag="tr", table_row_container="tbody",
-                 table_row_tag="tr", header_info_tag="th", table_info_tag="td"):
+                 table_row_tag="tr", header_info_tag="th", table_info_tag="td", keep_all_text=False,):
 
         self.soupscope = soupscope  # takes soup argument
         self.ignore_rows = ignore_rows  # takes soup argument which will be ignored
@@ -20,6 +21,7 @@ class TableScrape:
         self.table_row_tag = table_row_tag  # tag value of where table row info exists
         self.header_info_tag = header_info_tag # tag value of info stored inside header_row_tag
         self.table_info_tag = table_info_tag  # tag value of info stored inside table_row_tag
+        self.keep_all_text = keep_all_text # keeps first text if False, keeps all text separated by "|" if True
 
     # gets row info based on row tag
     def get_row_info(self, row, tag_to_find):
@@ -27,25 +29,44 @@ class TableScrape:
         row_data = row.find_all(tag_to_find)
         for column in range(len(row_data)):
             if column not in self.ignore_columns:
-                row_list.append(row_data[column].text)
+                column_text_list = row_data[column].find_all(text=True)
+                if self.keep_all_text:
+                    column_text = "|".join(column_text_list)
+                else:
+                    column_text = column_text_list[0]
+                row_list.append(column_text)
         return row_list
 
-    # gets table values depending on parameters given
-    def scrape(self):
-        table = []
-
-        rows = self.soupscope.find(self.table_row_container).find_all(self.table_row_tag)
+    # gets table header info
+    def get_header_info(self):
         if self.separated_header:
-            header_row = self.soupscope.find(self.header_row_container).find(self.header_row_tag)
+            header_row = self.soupscope.find(self.header_row_container)
         else:
-            header_row = self.soupscope.find(self.table_row_container).find(self.table_row_tag)
-            rows = rows[1:]
+            header_row = self.soupscope.find(self.table_row_container)
 
-        full_header = self.get_row_info(header_row, self.header_info_tag)
-        table.append(full_header)
+        header_row = header_row.find(self.header_row_tag)
+        full_header_data = self.get_row_info(header_row, self.header_info_tag)
+        return full_header_data
+
+    # gets table body info
+    def get_body_info(self, rows):
+        full_body_data = []
+        if not self.separated_header:
+            rows = rows[1:]
 
         for row in rows:
             if row not in self.ignore_rows:
                 row_data = self.get_row_info(row, self.table_info_tag)
-                table.append(row_data)
+                full_body_data.append(row_data)
+        return full_body_data
+
+    # gets table values depending on parameters given
+    def scrape(self):
+        table = []
+        rows = self.soupscope.find(self.table_row_container).find_all(self.table_row_tag)
+        full_header = self.get_header_info()
+        table.append(full_header)
+        full_body = self.get_body_info(rows)
+        for info_row in full_body:
+            table.append(info_row)
         return table
