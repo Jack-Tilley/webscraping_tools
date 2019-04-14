84bf541e9896a60ae8cf39d505a2c11d7e199a7f
Jack Tilley <47064848+Jack-Tilley@users.noreply.github.com>
2019-04-13 22:13:08 -0400

    Update listscraper.py

--- ListScrape/listscraper.py
+++ ListScrape/listscraper.py
@@ -6,12 +6,13 @@ class ListScrape:
         self.row_tag = row_tag # usually li value, list element
         self.rows_to_ignore = rows_to_ignore # list containing indices of rows to ignore
         self.columns_to_ignore = columns_to_ignore # list containing indices of columns to ignore
-
+    
+    # gets li element text
     def get_row_info(self, row):
         infolist = row.find_all(text=True)
         return [value for value in infolist if "\n" not in value]
 
-
+    # scrapes all wanted lis
     def scrape(self):
         table = []
         rows = self.soupscope.find_all(self.row_tag)
